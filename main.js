c4e238b64fbdf5f33adee6aff1df6709b81c8d7d
Orlando <orlandeno13@gmail.com>
2023-10-03 12:27:12 -0500

    cambios de código

--- main.js
+++ main.js
@@ -1,33 +1,54 @@
+document.getElementById("capital").addEventListener("input", function (event) {
+  const capital1 = event.target.value.replace(/\,/g, "");
+  event.target.value = capital1.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+});
+document.getElementById("button").addEventListener("click", function () {
+  var contenido = document.getElementById("foot");
+  if (contenido.style.display === "none") {
+    contenido.style.display = "block";
+  } else {
+    contenido.style.display = "block";
+  }
+});
 function calcularPrestamo() {
+  let capitalPagar,
+    capitalTotal,
+    interesesMensuales,
+    interesesPagar,
+    total,
+    totalPagar;
   document.getElementById("tab").innerHTML = "";
-  let capital = Number(document.getElementById("capital").value);
+  let capital2 = document.getElementById("capital").value.replace(/\,/g, "");
+  let capital = parseFloat(capital2);
   let cuota = Number(document.getElementById("cuota").value);
   let interes = Number(document.getElementById("interes").value);
-
   if (capital > 0) {
     for (num = 1; num <= cuota; num++) {
       capitalTotal = capital / cuota;
       capitalPagar = capitalTotal.toFixed(2);
-      intereses = ((capital / cuota) * interes) / 100;
-      interesesPagar = intereses.toFixed(2);
-      total = capitalTotal + intereses;
+      interesesMensuales = (capital * interes) / (100 * cuota);
+      interesesPagar = interesesMensuales.toFixed(2);
+      total = capitalTotal + interesesMensuales;
       totalPagar = total.toFixed(2);
-      document.getElementById("tab").innerHTML =
-        document.getElementById("tab").innerHTML +
-        `<tr>
-                        <td> ${num}</td>
-                        <td> ${capitalPagar}</td>
-                        <td> ${interesesPagar}</td>
-                        <td> ${totalPagar}</td>
-                    </tr>`;
+      document.getElementById("tab").innerHTML += `<tr>
+        <td>${num}</td>
+        <td>${capitalPagar}</td>
+        <td>${interesesPagar}</td>
+        <td>${totalPagar}</td>
+      </tr>`;
     }
-    capitalaPagar = capital.toFixed(2);
-    interesCuota = (intereses * cuota).toFixed(2);
-    totalCuota = (totalPagar * cuota).toFixed(2);
+    let capitalaPagar = capital.toFixed(2);
+    let interesCuota = (interesesMensuales * cuota).toFixed(2);
+    let totalCuota = (total * cuota).toFixed(2);
+
     document.getElementById("t1").innerHTML = capitalaPagar;
     document.getElementById("t2").innerHTML = interesCuota;
     document.getElementById("t3").innerHTML = totalCuota;
   } else {
-    alert("Falta ingresar un Número");
+    Swal.fire({
+      icon: "error",
+      title: "Información incompleta.",
+      text: "Parece ser que falta por ingresar algún número.",
+    });
   }
 }
